5cd1b89e97cf4425e46fd1bda5cc9aedc03d38e9
RomRL <romharel98@gmail.com>
2023-05-17 11:06:59 +0300

    update

--- fitness-web/server/src/routes/users.js
+++ fitness-web/server/src/routes/users.js
@@ -4,12 +4,12 @@ import { UserModel } from "../models/Users.js";
 
 const router = express.Router();   //Create Router
 
-router.use(express.json()); // Add this line to parse request bodies as JSON
 
 router.post('/register', async (req, res) => {
     const { email, password } = req.body;
+    console.log(email, password);
+    console.log("here" , req.body.email, req.body.password);
 
-    console.log("here" , req.body)
 
     const user= await UserModel.findOne(  {email: email }    );
     res.json( user);
