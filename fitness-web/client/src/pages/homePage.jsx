016f89c331227bea4fac9fddd2cfdf8ccefbf9d1
RomRL <romharel98@gmail.com>
2023-05-17 22:58:09 +0300

    Fixed WIFI LOGIN REGISTER

--- fitness-web/client/src/pages/homePage.jsx
+++ fitness-web/client/src/pages/homePage.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React from 'react';
+import MainLayout from '../layout/MainLayout.jsx';
 
-const homePage = () => {
-  return (
-    <div className='page'>homePage</div>
-  )
+export default function HomePage(props) {
+    const { name } = props;
+    //style
+    const style= {
+        
+        color:'blue',
+        fontSize:'18px',
+    }
+    // if (name.length >3) {
+    //     style.color='red'
+    // }
+//create new html page
+    return (
+      
+        <MainLayout>
+         
+            <div className="home-page">
+                <h1 style={style}>Welcome to my Fitness Web</h1>
+                <p>My name is {name}</p>
+                <p>My name is {name}</p>
+        
+            </div>
+        </MainLayout>
+    )
 }
-export default homePage;
+
+
