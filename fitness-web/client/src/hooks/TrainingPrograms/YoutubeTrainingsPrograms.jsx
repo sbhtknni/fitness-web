290d47c172cf6cafd5520686727f1c52a1b7129f
Daniel Maman <109150591+sbhtknni@users.noreply.github.com>
2023-05-17 11:16:46 +0300

    merge with rom

--- fitness-web/client/src/hooks/TrainingPrograms/YoutubeTrainingsPrograms.jsx
+++ fitness-web/client/src/hooks/TrainingPrograms/YoutubeTrainingsPrograms.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import YotubeTable from '../../componenets/TrainingProgramsComp/YotubeTable';
 
 const YoutubeTrainingsPrograms = () => {
     const [selectedCategory, setSelectedCategory] = useState('');
@@ -8,251 +9,136 @@ const YoutubeTrainingsPrograms = () => {
     };
 
     const getCategoryLinks = () => {
+        const back = [
+            [
+              {
+                src: 'https://www.youtube.com/embed/EMKrh3naOBg',
+                title: 'Back Video 1',
+              },
+              {
+                src: 'https://www.youtube.com/embed/YvKOvyiAdAI',
+                title: 'Back Video 2',
+              },
+            ],
+            [
+              {
+                src: 'https://www.youtube.com/embed/P9cnQAfhwIM',
+                title: 'Back Video 3',
+              },
+              {
+                src: 'https://www.youtube.com/embed/o062bxRT1EA',
+                title: 'Back Video 4',
+              },
+            ],
+        ];
+        const chest = [
+            [
+              {
+                src: 'https://www.youtube.com/embed/4o1YzksPuqg',
+                title: 'chest Video 1',
+              },
+              {
+                src: 'https://www.youtube.com/embed/NsEbXsTwas8',
+                title: 'chest Video 2',
+              },
+            ],
+            [
+              {
+                src: 'https://www.youtube.com/embed/jBhZWX91bec',
+                title: 'chest Video 3',
+              },
+              {
+                src: 'https://www.youtube.com/embed/wUOBow4rsSY',
+                title: 'chest Video 4',
+              },
+            ],
+        ];
+        const legs = [
+            [
+              {
+                src: 'https://www.youtube.com/embed/AQyhNSDXYf0',
+                title: 'legs Video 1',
+              },
+              {
+                src: 'https://www.youtube.com/embed/4MOW8mJSR4k',
+                title: 'legs Video 2',
+              },
+            ],
+            [
+              {
+                src: 'https://www.youtube.com/embed/TC8ui7WkOao',
+                title: 'legs Video 3',
+              },
+              {
+                src: 'https://www.youtube.com/embed/q7rCeOa_m58',
+                title: 'legs Video 4',
+              },
+            ],
+        ];
+        const shoulders = [
+            [
+              {
+                src: 'https://www.youtube.com/embed/9pILlZud3iY',
+                title: 'shoulders Video 1',
+              },
+              {
+                src: 'https://www.youtube.com/embed/Aq7qermvmXM',
+                title: 'shoulders Video 2',
+              },
+            ],
+            [
+              {
+                src: 'https://www.youtube.com/embed/KBujwXdONwo',
+                title: 'shoulders Video 3',
+              },
+              {
+                src: 'https://www.youtube.com/embed/rnDWTXYDMOg',
+                title: 'shoulders Video 4',
+              },
+            ],
+        ];
+        const arms = [
+            [
+              {
+                src: 'https://www.youtube.com/embed/UY6-JzdnHUM',
+                title: 'legs Video 1',
+              },
+              {
+                src: 'https://www.youtube.com/embed/kiI57DPH3fM',
+                title: 'legs Video 2',
+              },
+            ],
+            [
+              {
+                src: 'https://www.youtube.com/embed/bXItN5LESkw',
+                title: 'legs Video 3',
+              },
+              {
+                src: 'https://www.youtube.com/embed/9rVxPC6IokA',
+                title: 'legs Video 4',
+              },
+            ],
+        ];
         switch (selectedCategory) {
             case 'back':
                 return (
-                    <div>
-                        <table>
-                            <tbody>
-                                <tr>
-                                    <td>
-                                        <iframe
-                                            width="500"
-                                            height="300"
-                                            src="https://www.youtube.com/embed/EMKrh3naOBg"
-                                            title="Back Video 1"
-                                            allowFullScreen
-                                        ></iframe>
-                                    </td>
-                                    <td>
-                                        <iframe
-                                            width="500"
-                                            height="300"
-                                            src="https://www.youtube.com/embed/YvKOvyiAdAI"
-                                            title="Back Video 2"
-                                            allowFullScreen
-                                        ></iframe>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <iframe
-                                            width="500"
-                                            height="300"
-                                            src="https://www.youtube.com/embed/P9cnQAfhwIM"
-                                            title="Back Video 3"
-                                            allowFullScreen
-                                        ></iframe>
-                                    </td>
-                                    <td>
-                                        <iframe
-                                            width="500"
-                                            height="300"
-                                            src="https://www.youtube.com/embed/o062bxRT1EA"
-                                            title="Back Video 4"
-                                            allowFullScreen
-                                        ></iframe>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
+                    <YotubeTable videos={back} />
                 );
             case 'chest':
                 return (
-                    <div>
-                        <table>
-                            <tbody>
-                                <tr>
-                                    <td>
-                                        <iframe
-                                            width="500"
-                                            height="300"
-                                            src="https://www.youtube.com/embed/4o1YzksPuqg"
-                                            title="Chest Video 1"
-                                            allowFullScreen
-                                        ></iframe>
-                                    </td>
-                                    <td>
-                                        <iframe
-                                            width="500"
-                                            height="300"
-                                            src="https://www.youtube.com/embed/NsEbXsTwas8"
-                                            title="Chest Video 2"
-                                            allowFullScreen
-                                        ></iframe>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <iframe
-                                            width="500"
-                                            height="300"
-                                            src="https://www.youtube.com/embed/jBhZWX91bec"
-                                            title="Chest Video 3"
-                                            allowFullScreen
-                                        ></iframe>
-                                    </td>
-                                    <td>
-                                        <iframe
-                                            width="500"
-                                            height="300"
-                                            src="https://www.youtube.com/embed/wUOBow4rsSY"
-                                            title="Chest Video 4"
-                                            allowFullScreen
-                                        ></iframe>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
+                    <YotubeTable videos={chest} />
                 );
             case 'legs':
                 return (
-                    <div>
-                    <table>
-                        <tbody>
-                            <tr>
-                                <td>
-                                    <iframe
-                                        width="500"
-                                        height="300"
-                                        src="https://www.youtube.com/embed/AQyhNSDXYf0"
-                                        title="legs Video 1"
-                                        allowFullScreen
-                                    ></iframe>
-                                </td>
-                                <td>
-                                    <iframe
-                                        width="500"
-                                        height="300"
-                                        src="https://www.youtube.com/embed/4MOW8mJSR4k"
-                                        title="legs Video 2"
-                                        allowFullScreen
-                                    ></iframe>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <iframe
-                                        width="500"
-                                        height="300"
-                                        src="https://www.youtube.com/embed/TC8ui7WkOao"
-                                        title="legs Video 3"
-                                        allowFullScreen
-                                    ></iframe>
-                                </td>
-                                <td>
-                                    <iframe
-                                        width="500"
-                                        height="300"
-                                        src="https://www.youtube.com/embed/q7rCeOa_m58"
-                                        title="legs Video 4"
-                                        allowFullScreen
-                                    ></iframe>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
+                    <YotubeTable videos={legs} />
                 );
             case 'arms':
                 return (
-                    <div>
-                    <table>
-                        <tbody>
-                            <tr>
-                                <td>
-                                    <iframe
-                                        width="500"
-                                        height="300"
-                                        src="https://www.youtube.com/embed/UY6-JzdnHUM"
-                                        title="arms Video 1"
-                                        allowFullScreen
-                                    ></iframe>
-                                </td>
-                                <td>
-                                    <iframe
-                                        width="500"
-                                        height="300"
-                                        src="https://www.youtube.com/embed/kiI57DPH3fM"
-                                        title="arms Video 2"
-                                        allowFullScreen
-                                    ></iframe>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <iframe
-                                        width="500"
-                                        height="300"
-                                        src="https://www.youtube.com/embed/bXItN5LESkw"
-                                        title="arms Video 3"
-                                        allowFullScreen
-                                    ></iframe>
-                                </td>
-                                <td>
-                                    <iframe
-                                        width="500"
-                                        height="300"
-                                        src="https://www.youtube.com/embed/9rVxPC6IokA"
-                                        title="arms Video 4"
-                                        allowFullScreen
-                                    ></iframe>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
+                    <YotubeTable videos={arms} />
                 );
             case 'shoulders':
                 return (
-                    <div>
-                    <table>
-                        <tbody>
-                            <tr>
-                                <td>
-                                    <iframe
-                                        width="500"
-                                        height="300"
-                                        src="https://www.youtube.com/embed/9pILlZud3iY"
-                                        title="shoulders Video 1"
-                                        allowFullScreen
-                                    ></iframe>
-                                </td>
-                                <td>
-                                    <iframe
-                                        width="500"
-                                        height="300"
-                                        src="https://www.youtube.com/embed/Aq7qermvmXM"
-                                        title="shoulders Video 2"
-                                        allowFullScreen
-                                    ></iframe>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <iframe
-                                        width="500"
-                                        height="300"
-                                        src="https://www.youtube.com/embed/KBujwXdONwo"
-                                        title="shoulders Video 3"
-                                        allowFullScreen
-                                    ></iframe>
-                                </td>
-                                <td>
-                                    <iframe
-                                        width="500"
-                                        height="300"
-                                        src="https://www.youtube.com/embed/rnDWTXYDMOg"
-                                        title="shoulders Video 4"
-                                        allowFullScreen
-                                    ></iframe>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
+                    <YotubeTable videos={shoulders} />
                 );
             default:
                 return null;
