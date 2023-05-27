8cdc2ace2b673d9f311f5ebca46ddfb88e0aee13
Daniel Maman <109150591+sbhtknni@users.noreply.github.com>
2023-05-27 19:17:16 +0300

    muscleInformation

--- fitness-web/client/src/pages/TrainingPrograms.jsx
+++ fitness-web/client/src/pages/TrainingPrograms.jsx
@@ -1,48 +1,69 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import MainLayout from '../layout/MainLayout.jsx';
 import YoutubeTrainingsPrograms from '../hooks/TrainingProgramsHook/YoutubeTrainingsPrograms.jsx';
 import TrainingInformation from '../componenets/TrainingProgramsComp/TrainingInformation.jsx';
+import axios from 'axios';
+
 
 
 const TrainingProgramas = () => {
-    
+    const [muscle, setMuscle] = useState();
+    const [generalInformation, setGeneralInformation] = useState();
+    const [topics, setTopics] = useState([]);
+    const [topic, setTopicsVals] = useState([]);
+    const [information, setInformation] = useState([]);
+    const [link, setLink] = useState([]);
 
+    useEffect(() => {
+        const fetchmuscleInformation = async () => {
+            try {
+                const response = await axios.get(`http://localhost:3002/muscle/${localStorage.getItem('selectedTrainingInfo')}`);
+                const data = response.data;
 
-    
+                setMuscle(data.musclesInformation.muscle);
+                setGeneralInformation(data.musclesInformation.generalInformation);
+                setTopics(data.musclesInformation.topics);
+                setTopicsVals(data.musclesInformation.topics.map((info) => info.topic))
+                setInformation(data.musclesInformation.topics.map((info) => info.information))
+                setLink(data.musclesInformation.topics.map((info) => info.link))
+            } catch (error) {
+                console.error('Error fetching muscleInformation:', error);
+            }
+        };
+
+        fetchmuscleInformation();
+    }, []);
+
     return (
         <MainLayout>
-        
+
             <body className="vsc-initialized">
                 <main role="main">
                     <div className="jumbotron">
                         <div className="container">
-                            <h1 className="display-3">Hello, world!</h1>
-                            <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+                            <h1 className="display-3">Muscle {muscle}</h1>
+                            <p>{generalInformation} </p>
                             <p><a className="btn btn-primary btn-lg" href="#" role="button">Learn more »</a></p>
                         </div>
                     </div>
-
                     <div className="container">
                         <div className="row">
-                            <div className="col-md-4">
-                                <h2>Heading</h2>
-                                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
-                                <p><a className="btn btn-secondary" href="#" role="button">View details »</a></p>
-                            </div>
-                            <div className="col-md-4">
-                                <h2>Heading</h2>
-                                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
-                                <p><a className="btn btn-secondary" href="#" role="button">View details »</a></p>
-                            </div>
-                            <div className="col-md-4">
-                                <h2>Heading</h2>
-                                <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-                                <p><a className="btn btn-secondary" href="#" role="button">View details »</a></p>
-                            </div>
+                            {topic.map((topicItem, index) => (
+                                <div className="col-md-4" key={index}>
+                                    <h2>{topicItem}</h2>
+                                    <p>{information[index]}</p>
+                                    {link[index] ? (
+                                        <p>
+                                            <a className="btn btn-secondary" href={link[index]} role="button">
+                                                View details »
+                                            </a>
+                                        </p>
+                                    ) : null}
+                                </div>
+                            ))}
                         </div>
-
                         <hr />
-                    </div>
+                    </div>    
                 </main>
 
                 <footer className="container">
