462139c6c7069ab5fc40b0b333fd740a8ff709d6
RomRL <romharel98@gmail.com>
2023-06-01 20:13:51 +0300

    last one

--- fitness-web/server/src/routes/validate.js
+++ fitness-web/server/src/routes/validate.js
@@ -7,7 +7,6 @@ import jwt from "jsonwebtoken";
 
 export const validateToken = (req, res, next) => {
     const token =req.headers.authorization;
-    console.log("Validate token",token);
 
     if (!token) {
         console.log("Access denied, no token provided");
@@ -16,16 +15,10 @@ export const validateToken = (req, res, next) => {
     try {
         //remove the "Bearer " from the token
         const tokenWithoutBearer = token.split(" ")[1];
-
         const verified = jwt.verify(tokenWithoutBearer, "secret");
-        console.log("verified",verified);
         req.user = verified;
-        console.log("User is logged in");
-
         next();
     } catch (error) {
-        console.log("! User is logged in");
-
         res.status(400).json({ message: "Invalid token" });
     }
     }
