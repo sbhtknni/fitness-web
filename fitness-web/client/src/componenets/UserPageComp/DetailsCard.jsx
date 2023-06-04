b00f1252607214d989d32bc0e33220024986dadb
RomRL <romharel98@gmail.com>
2023-06-04 21:58:25 +0300

    New Error Page , Start To Work On User Page Again

---
+++ fitness-web/client/src/componenets/UserPageComp/DetailsCard.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import {
+  MDBCard,
+  MDBCardBody,
+  MDBCardImage,
+  MDBBtn,
+  MDBCol,
+} from "mdb-react-ui-kit";
+import RowOfDetails from "./RowOfDetails";
+
+export default function DetailsCard(props) {
+  const { user } = props;
+  return (
+    <MDBCard className= " mb-3 d-flex align-items-startmb-6">
+      <MDBCardBody>
+        <RowOfDetails
+          type="Full Name"
+          value={user.firstName + " " + user.lastName}></RowOfDetails>
+        <RowOfDetails type=" Email " value={user.email}></RowOfDetails>
+        <RowOfDetails type=" Height " value={user.height}></RowOfDetails>
+        <RowOfDetails type=" Weight " value={user.weight}></RowOfDetails>
+      </MDBCardBody>
+    </MDBCard>
+  );
+}
