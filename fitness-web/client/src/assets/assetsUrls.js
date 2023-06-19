c89e97e5bc3cb90c56be06b9ae8d3630777ee0b7
RomRL <romharel98@gmail.com>
2023-06-19 12:27:11 +0300

    assets

--- fitness-web/client/src/assets/assetsUrls.js
+++ fitness-web/client/src/assets/assetsUrls.js
@@ -2,6 +2,10 @@ const allURLS = {
 
         "logo": "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
         "logo2": "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+        "logo3": "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+        "logo4": "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+        "logo5": "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+        
 
 }
 
