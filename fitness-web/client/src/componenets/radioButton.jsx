832130ba2ed536fdde786e4301bc43ac9db99208
RomRL <romharel98@gmail.com>
2023-06-19 14:38:30 +0300

    Radio Button

--- fitness-web/client/src/componenets/radioButton.jsx
+++ fitness-web/client/src/componenets/radioButton.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { MDBBtnGroup, MDBRadio } from 'mdb-react-ui-kit';
 function RadioButton(props) {
   const { options, selectedOption, onOptionChange } = props;
 
@@ -10,24 +10,41 @@ function RadioButton(props) {
   };
 
   return (
-    <div className="btn-group btn-group-toggle" data-toggle="buttons">
-      {options.map((option) => (
-        <label
-          key={option}
-          className={`btn btn-secondary ${option === selectedOption ? 'active' : ''}`}
-          onClick={() => handleOptionChange(option)}
-        >
-          <input
-            type="radio"
-            name="options"
-            autoComplete="off"
+    // <><div className="btn-group btn-group-toggle" data-toggle="buttons">
+    //   {options.map((option) => (
+    //     <label
+    //       key={option}
+    //       className={`btn btn-secondary ${option === selectedOption ? 'active' : ''}`}
+    //       onClick={() => handleOptionChange(option)}
+    //     >
+    //       <input
+    //         type="radio"
+    //         name="options"
+    //         autoComplete="off"
+    //         checked={option === selectedOption}
+    //         onChange={() => { } } />{' '}
+    //       {option}
+    //     </label>
+    //   ))}
+      
+    // </div>
+    <MDBBtnGroup>
+        {options.map((option) => (
+          <MDBRadio
+            key={option}
+            btn
+            btnColor='dark'
+            id={`btn-radio${option}`}
+            name='options'
+            wrapperClass='mx-2'
+            wrapperTag='span'
+            label={option}
             checked={option === selectedOption}
-            onChange={() => {}}
-          />{' '}
-          {option}
-        </label>
-      ))}
-    </div>
+            onChange={() => handleOptionChange(option)}
+          />
+        ))}
+      </MDBBtnGroup>
+
   );
 }
 
