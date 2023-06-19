ed4ac296fbdd709506aa9b362230737bff08b327
RomRL <romharel98@gmail.com>
2023-06-19 17:09:08 +0300

    bestUserPage

--- fitness-web/client/src/componenets/AuthPageComp/LoginComponent.jsx
+++ fitness-web/client/src/componenets/AuthPageComp/LoginComponent.jsx
@@ -66,12 +66,11 @@ function LoginComponent() {
     return (
         <MainLayout>
             <MDBContainer fluid onSubmit={handleSubmit}>
-                <div className="p-5 bg-image" style={{ backgroundImage: 'url(https://mdbootstrap.com/img/new/textures/full/171.jpg)', borderRadius: '10px', margin: '2vh', height: '300px' }}></div>
-                <div style={{  backgroundImage: `url(${background})`, borderRadius: '10px', padding: '2vh', margin: '9vh' }} >
+                <div style={{  backgroundImage: `url(${background})`, borderRadius: '10px', padding: '2vh' }} >
 
                 <MDBRow className='d-flex justify-content-center align-items-center h-100'>
                     <MDBCol col='12'>
-                        <MDBCard className='bg-dark text-white mx-auto' style={{ borderRadius: '1rem', maxWidth: '400px', marginTop: '-160px', background: 'hsla(0, 0%, 100%, 0.8)', backdropFilter: 'blur(30px)' }} >
+                        <MDBCard className='bg-dark text-white mx-auto' style={{ borderRadius: '1rem' ,maxWidth: '400px', background: 'hsla(0, 0%, 100%, 0.8)', backdropFilter: 'blur(30px)' }} >
                             <MDBCardBody className='p-5 d-flex flex-column align-items-center mx-auto w-100'>
                                 <h2 className="fw-bold mb-2 text-uppercase">Login</h2>
                                 <p className="text-white-50 mb-5">Please enter your login and password!</p>
@@ -84,8 +83,7 @@ function LoginComponent() {
                     </MDBCol>
                 </MDBRow>
                 <LoginModal showModal={showModal} modalOption={modalOption} modalMessage={modalMessage} handleModalClose={handleModalClose} setShowModal={setShowModal} />
-                <br />
-                <br />
+        
                 </div>
                     <hr />
                     <Footer />
