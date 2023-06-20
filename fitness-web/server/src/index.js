8d9edb4b07addd8a85bea6bccb662dfc41f263a2
RomRL <romharel98@gmail.com>
2023-06-21 01:23:17 +0300

    ss

--- fitness-web/server/src/index.js
+++ fitness-web/server/src/index.js
@@ -27,5 +27,5 @@ mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
     console.error('Error connecting to MongoDB:', error);
   });
 
-app.listen( ); //Start Server
+app.listen(5000, () => console.log('Server Running on Port 5000')); //Start Server
 
